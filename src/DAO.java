c59a1d8a75c598420237ee09f032932ec7876867
Lucas Silveira <lucas.silveira.2010@hotmail.com>
2018-03-28 19:01:04 -0300

    Adicionado arquivo.txt

--- src/DAO.java
+++ src/DAO.java
@@ -4,22 +4,6 @@ import java.io.IOException;
 
 public class DAO {
 
-    public static void main(String[] args) {
-        try {
-            String[] a = lerPedidos();
-            for (String pedido : a) {
-                String[] gambi = pedido.split(";");
-                String nomeCliente = gambi[0];
-                float peso = Float.parseFloat(gambi[1]);
-                float precoPorKg = Float.parseFloat(gambi[2]);
-                System.out.println("Nome: " + nomeCliente + " | Peso: " + peso + " | Preço por Kg: " + precoPorKg );
-            }
-        }
-        catch (IOException e) {
-            System.err.println("Ops");
-        }
-}
-
     public static String[] lerPedidos() throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("arquivo.txt"));
 
--- src/Fila.java
+++ src/Fila.java
@@ -1,9 +1,22 @@
+import java.io.IOException;
+
 public class Fila {
 
     public static void main(String[] args) {
-
-        System.out.println("Olá Jorge");
-
+        try {
+            String[] a = DAO.lerPedidos();
+            for (String pedido : a) {
+                String[] gambi = pedido.split(";");
+                String nomeCliente = gambi[0];
+                float peso = Float.parseFloat(gambi[1]);
+                float precoPorKg = Float.parseFloat(gambi[2]);
+                System.out.println("Nome: " + nomeCliente + " | Peso: " + peso + " | Preço por Kg: " + precoPorKg );
+            }
+        }
+        catch (IOException e) {
+            System.err.println("Ops");
+            e.printStackTrace();
+        }
     }
 
 }
